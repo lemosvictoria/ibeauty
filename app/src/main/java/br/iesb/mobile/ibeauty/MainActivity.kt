6ce172240a9e9227ffa5fc45c509c9d7114fb1fd
lemosvictoria <victorialuisatl@gmail.com>
2021-04-13 22:43:30 -0300

    Tela de tipo de cadastro(cliente ou profissional)

--- app/src/main/java/br/iesb/mobile/ibeauty/MainActivity.kt
+++ app/src/main/java/br/iesb/mobile/ibeauty/MainActivity.kt
@@ -1,11 +1,18 @@
 package br.iesb.mobile.ibeauty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btIrPara.setOnClickListener {
+            val intencaoDeChamada = Intent(this, TipoCadastro::class.java)
+            startActivity(intencaoDeChamada)
+        }
     }
 }
---
+++ app/src/main/java/br/iesb/mobile/ibeauty/TipoCadastro.kt
@@ -0,0 +1,19 @@
+package br.iesb.mobile.ibeauty
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tipo_cadastro.*
+
+class TipoCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tipo_cadastro)
+
+    ivVoltarTela.setOnClickListener {
+        val intencaoDeChamada = Intent(this, MainActivity::class.java)
+        startActivity(intencaoDeChamada)
+    }
+
+    }
+}
