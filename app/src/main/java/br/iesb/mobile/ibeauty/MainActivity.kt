85e47a9089aefa5a00a5eb4afb5c13592667156b
mouramillena <millenamouramms@gmail.com>
2021-04-18 23:36:43 -0300

    Criação das Telas de Cadastro de Profissional

--- app/src/main/java/br/iesb/mobile/ibeauty/MainActivity.kt
+++ app/src/main/java/br/iesb/mobile/ibeauty/MainActivity.kt
@@ -1,11 +1,19 @@
 package br.iesb.mobile.ibeauty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btCadastroProf.setOnClickListener {
+            val intencaoChamada = Intent (this, CadastroProfissional1::class.java)
+            startActivity(intencaoChamada)
+        }
     }
+
 }
