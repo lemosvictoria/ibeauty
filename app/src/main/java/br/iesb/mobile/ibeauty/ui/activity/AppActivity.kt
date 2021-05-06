b673680cdd940f1216f72e713bb2beec1a65bebb
Geovascordeiro <geovanacordeiro1765@gmail.com>
2021-05-06 01:34:00 -0300

    Iniciando navegação btlogin

--- app/src/main/java/br/iesb/mobile/ibeauty/ui/activity/AppActivity.kt
+++ app/src/main/java/br/iesb/mobile/ibeauty/ui/activity/AppActivity.kt
@@ -2,11 +2,20 @@ package br.iesb.mobile.ibeauty.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 import br.iesb.mobile.ibeauty.R
+import br.iesb.mobile.ibeauty.ui.fragment.HomeFragment
+import br.iesb.mobile.ibeauty.ui.fragment.LoginFragment
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fundoApp, HomeFragment(), "Home Fragment")
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
     }
 }
